ad72acccb47c2a28bc49b7a86c63eb05b2a65ab9
Richard Freitas <wesleyrichard30@gmail.com>
2024-08-29 08:22:34 -0300

    refactor: improve and fix all server and client code

--- client.py
+++ client.py
@@ -6,79 +6,44 @@ class Client:
     def __init__(self, host="127.0.0.1", port=55555):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, port))
+        self.nickname = None
+        self.running = True
 
     def send_message(self, message):
         self.client.send(message.encode("utf-8"))
 
-    def receive_messages(self, client):
-        while True:
-            try:
-                message = client.recv(1024).decode("utf-8")
-                if message:
-                    self.display_message(message)
-                else:
-                    break
-            except:
-                client.close()
-                break
-
     def display_message(self, message):
         # Here can be used to change the displayed message format
         match message.split(" ")[0]:
-            case "!users":
-                # _, users_count, users = message.split(" ", 2)
-                # print(f"Users online: {users_count} - {users.replace(' ', ', ')}")
-                print(message)
-            case "!msg":
-                # _, sender, msg_content = message.split(" ", 2)
-                # print(f"{sender}: {msg_content}")
-                print(message)
-            case "!changenickname":
-                # _, old_nick, new_nick = message.split(" ", 2)
-                # print(f"{old_nick} changed nickname to {new_nick}")
-                print(message)
-            case "!poke":
-                # _, poker_nick, target_nick = message.split(" ", 2)
-                # print(f"{poker_nick} poked {target_nick}")
-                print(message)
             case _:
                 print(message)
 
+    def receive_messages(self, client):
+        while self.running:
+            message = client.recv(1024).decode("utf-8")
+            if message == "!nick":
+                nickname = input(f"{message} ")
+                self.client.send(f"!nick {nickname}".encode("utf-8"))
+                self.nickname = nickname
+            elif message.split(" ")[0] == "!exit":
+                self.display_message(message)
+                self.running = False
+                break
+            elif message:
+                self.display_message(message)
+
     def connect(self):
-        nickname = input("Choose your nickname: ")
-
-        if not nickname:
-            print("Invalid nickname, connection lost!")
-            self.client.close()
-            return
-
         try:
-            self.client.send(f"!nick {nickname}".encode("utf-8"))
-
             thread = threading.Thread(target=self.receive_messages, args=(self.client,))
             thread.start()
+
+            while self.running:
+                if self.nickname:
+                    message = input()
+                    self.send_message(message)
         except:
-            print("Connection lost!")
             self.client.close()
             return
-
-        while True:
-            try:
-                message = input()
-
-                match message.split(" ")[0]:
-                    case "!sendmsg":
-                        self.send_message(message)
-                    case "!changenickname":
-                        self.send_message(message)
-                    case "!poke":
-                        self.send_message(message)
-                    case _:
-                        print("Invalid command!")
-            except:
-                print("Connection lost!")
-                self.client.close()
-                break
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from asyncio import constants
 import socket
 import threading
 
@@ -21,22 +22,33 @@ class Server:
         welcome_message = f"!users {len(nicknames)} " + " ".join(
             nicknames.values()
         )
+
+        for c in clients:
+            if c != client:
+                c.send(f"!join {nickname}".encode("utf-8"))
+
         client.send(welcome_message.encode("utf-8"))
 
     def change_nickname(self, client, old_nick, new_nick):
         nicknames[client] = new_nick
+
         self.broadcast(
             f"!changenickname {old_nick} {new_nick}".encode("utf-8")
         )
-    
+
     def poke_user(self, client, target_nick):
         if target_nick in nicknames.values():
             poker_nick = nicknames[client]
+
+            if poker_nick == target_nick:
+                client.send("!poke you can't poke yourself".encode("utf-8"))
+                return
+
             self.broadcast(
                 f"!poke {poker_nick} {target_nick}".encode("utf-8")
             )
         else:
-            client.send("User not found!".encode("utf-8"))
+            client.send("!poke user not found".encode("utf-8"))
 
     def handle_client(self, client):
         while True:
@@ -45,27 +57,76 @@ class Server:
 
                 match message.split(" ")[0]:
                     case "!nick":
+                        if client in clients:
+                            client.send(
+                                "!nick already set, use !changenickname".encode("utf-8")
+                            )
+                            continue
+
+                        if (
+                            len(message.split(" ")) != 2
+                            or not message.split(" ")[1].strip()
+                        ):
+                            client.send("!exit invalid nick".encode("utf-8"))
+
+                            self.remove_client(client)
+                            break
+
+                        clients.append(client)
                         nickname = message.split(" ")[1]
-                        self.set_client(client, nickname)  
+
+                        self.set_client(client, nickname)
+
                     case "!sendmsg":
+                        if (
+                            len(message.split(" ")) < 2
+                            or not message.split(" ")[1].strip()
+                        ):
+                            client.send("!sendmsg invalid".encode("utf-8"))
+                            continue
+
                         msg_content = message.split(" ", 1)[1]
+
                         self.broadcast(
                             f"!msg {nicknames[client]} {msg_content}".encode("utf-8")
                         )
                     case "!changenickname":
+                        if (
+                            len(message.split(" ")) != 2
+                            or not message.split(" ")[1].strip()
+                        ):
+                            client.send("!changenickname invalid".encode("utf-8"))
+                            continue
+
                         old_nick = nicknames[client]
-                        new_nick = message.split(" ")[2]
+                        new_nick = message.split(" ")[1]
+
                         self.change_nickname(client, old_nick, new_nick)
                     case "!poke":
+                        if (
+                            len(message.split(" ")) != 2
+                            or not message.split(" ")[1].strip()
+                        ):
+                            client.send("!poke invalid".encode("utf-8"))
+                            continue
+
                         target_nick = message.split(" ")[1]
+
                         self.poke_user(client, target_nick)
                     case _:
-                        client.send("Invalid command!".encode("utf-8"))
-            except:
-                clients.remove(client)
-                client.close()
-                del nicknames[client]
+                        client.send("!cmd invalid command".encode("utf-8"))
+            except ConnectionResetError:
+                self.remove_client(client)
                 break
+
+    def remove_client(self, client):
+        if client in clients:
+            clients.remove(client)
+            nickname = nicknames.pop(client, None)
+
+            if nickname:
+                self.broadcast(f"!leave {nickname}".encode("utf-8"))
+            client.close()
 
     def start(self):
         print("Server is listening...")
@@ -75,11 +136,12 @@ class Server:
                 client, address = self.server.accept()
                 print(f"Connected with {str(address)}")
 
-                clients.append(client)
+                client.send("!nick".encode("utf-8"))
+
                 thread = threading.Thread(target=self.handle_client, args=(client,))
                 thread.start()
             except:
-                print("An error occurred!")
+                print("An error occurred, server closed!")
                 self.server.close()
 
 
